4e879e8d94b7c71b3536882429d9fb805715f609
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-04-14 15:07:32 -0700

    fix(tremor): jsx

--- packages/tremor/tsup.config.ts
+++ packages/tremor/tsup.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((opts) => ({
-  entryPoints: ["src/index.ts"],
-  format: ["cjs"],
+  format: ["cjs", "esm"],
   clean: !opts.watch,
   dts: true,
   outDir: "dist",
   target: "es2017",
+  splitting: true,
+  sourcemap: true,
+  minify: true,
+  entry: ["src/index.ts"],
 }));
