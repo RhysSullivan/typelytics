721407854876a8fbd83bccc96267c01a090ee3fd
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-04-14 14:20:16 -0700

    fix(tremor): bundle react correctly

--- packages/tremor/tsup.config.ts
+++ packages/tremor/tsup.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((opts) => ({
-  entryPoints: ["src/index.ts"],
+  entry: ['src/index.ts'],
   format: ["cjs", "esm"],
   clean: !opts.watch,
   dts: true,
   outDir: "dist",
   target: "es2017",
+  treeshake: true,
+  sourcemap: 'inline',
+  minify: true,
+  splitting: false,
+  external: ['react'],
+  injectStyle: false,
 }));
